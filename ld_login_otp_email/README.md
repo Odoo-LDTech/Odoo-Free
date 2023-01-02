d45b5c4fa6cd471a95d38ac49c50a6b74b9abb7b
LDTECH <raghukandasamy15@gmail.com>
2023-01-02 16:51:01 +0530

    [ADD]Adding new module

---
+++ ld_login_otp_email/README.md
@@ -0,0 +1,27 @@
+# Livedigital Technologies Private Limited
+
+* Steps to use this module.
+
+This module adds capability of email otp based login inside Odoo ERP.
+
+-------------------
+
+
+# Dependencies
+
+1. Odoo mail server setup.
+
+
+-------------------
+
+Usage
+
+Step 
+
+1. Install this module.
+
+2. Go into Configurations > Enable "Enable OTP Login" flag and fill "OTP Expire Duration" values.
+
+3. Logout and check by logging in again.
+
+------------------
---
+++ ld_login_otp_email/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Email OTP based users login',
+    'author': 'Livedigital Technologies Private Limited',
+    'summary': 'This module lets your Odoo users enable login with email OTP.',
+    'description': 'This module lets your Odoo users enable login with email OTP.',
+    'version': "15.0.0.1",
+    # 'price': '15.0',
+    # 'currency': 'USD', 
+    'license': 'LGPL-3',
+    'category': 'Extra Tools','Productivity'
+    'depends': ['base', 'web', 'mail'],
+    'data': [
+        'data/email_otp_template.xml',
+        'views/web_tmpl_enter_otp.xml',
+        'views/config_settings.xml'
+     ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'auto_install': False,
+    'website': 'https://ldtech.in',
+    'live_test_url' : 'https://www.youtube.com/watch?v=1EhtYYdz8Ew'
+}
