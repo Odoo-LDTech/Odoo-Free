698efe29c958b4a8a5b6afd3723eb0bc225cff9f
LDTech <odoo.ldtech@gmail.com>
2023-04-11 09:32:38 +0530

    [ADD]Adding new module for attendance

---
+++ ld_hr_attendance/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'LD Odoo HRMS Grace Time',
+    'author': 'Livedigital Technologies Private Limited',
+    'company': 'Livedigital Technologies Private Limited',
+    'maintainer': 'Livedigital Technologies Private Limited',
+    'website': 'https://ldtech.in',
+    'summary': '''The module offers grace time configuration
+                  inside Odoo HRMS Standard flow.''',
+    'description': 'LD HR Attendance.',
+    'depends': ['base', 'hr_attendance'],
+    'category': 'Human Resources',
+    'data': ['views.xml'],
+    'version': '16.0.0.0',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
