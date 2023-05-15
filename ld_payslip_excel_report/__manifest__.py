1e04f02fc543f1b82eec2ac5d5bce28ddbabde31
LDTech <odoo.ldtech@gmail.com>
2023-05-15 12:43:48 +0530

    Add new module

--- ld_payslip_excel_report/__manifest__.py
+++ ld_payslip_excel_report/__manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# This file is part of OpenERP. The COPYRIGHT file at the top level of
-# this module contains the full copyright notices and license terms.
+
 {
     'name': 'Payslip Excel Report',
     'version': "16.0.0.1",
