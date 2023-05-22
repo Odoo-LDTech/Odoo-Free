ebfbe71f3db0b7e13d526b4cd75a95672bb397a0
LDTech <odoo.ldtech@gmail.com>
2023-05-22 11:18:04 +0530

    Add new changes

--- ld_website_password_eye/__manifest__.py
+++ ld_website_password_eye/__manifest__.py
@@ -4,9 +4,11 @@
     "category": "Website",
     "author": "Livedigital Technologies Private Limited",
     "summary": "Website Password Eye",
+    "license": "LGPL-3",
     "depends": ["web", "website", "auth_signup"],
     "data": [
         "views/templates.xml",
     ],
     "installable": True,
+    'images': ['static/description/icon.png'],
 }
