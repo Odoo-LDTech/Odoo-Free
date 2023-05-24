4ec82f189ff9c974f14f9d70ae3a70849013760b
LDTech <odoo.ldtech@gmail.com>
2023-05-24 15:11:58 +0530

    Add new changes

---
+++ ld_website_reset_filter/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Reset Filter",
+    'version': "14.0.0.1",
+    'summary': """Reset Filter From Shop Page""",
+    'description': """This Module have the functionality by which customers can reset all the filters(attributes filter, category filter).""",
+    'author': "Livedigital Technologies Private Limited",
+    'website': "ldtech.in",
+    'category': 'Website',
+    "license": "LGPL-3",
+    'depends': ['website_sale'],
+    'data': [
+        'views/template_website_reset_filter.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': False,
+    'live_test_url' : 'https://www.youtube.com/watch?v=Yra14IxmFY4'
+}
