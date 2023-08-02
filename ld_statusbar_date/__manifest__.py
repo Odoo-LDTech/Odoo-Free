fdd8d9b7451d3ecc4fcb8d6d5b61fdda49d41b9b
Raghupathy k <raghupathy.k@ldtech.in>
2023-08-02 15:20:57 +0530

    [ADD]Raghu-Adding image path in manifest file

--- ld_statusbar_date/__manifest__.py
+++ ld_statusbar_date/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'LD - StatusBar Date',
+    'name': 'Status Bar Date',
     'version': '1.0',
     'summary': 'Highlights No of days the document stay in particular state.',
     'description': """Highlights No of days the document stay 
@@ -11,8 +11,10 @@
     'maintainer': 'Livedigital Technologies Private Limited',
     'website': 'https://ldtech.in',
     'depends': ['web', 'sale'],
+    'license': 'LGPL-3',
     'installable': True,
     'data': ['views/sale_order_view.xml'],
+    'images': ['static/description/icon.jpg'],
     'assets': {
         'web.assets_backend': [
             'ld_statusbar_date/static/src/**/*',
