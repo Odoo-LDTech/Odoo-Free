8492463471d30e0c83cc7cae120bd21db37a6cd3
Raghupathy k <raghupathy.k@ldtech.in>
2023-08-10 15:01:15 +0530

    [ADD]New module for Login page

---
+++ ld_login_background/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Login Background And Styles',
+    'summary': 'The new configurable Odoo Web Login Screen',
+    'version': '16.0.0.1',
+    'category': 'website',
+    'summary': """
+    You can customised login page like add background image or color and change position of login form.
+    """,
+    'author': 'Livedigital Technologies Private Limited',
+    'company': 'Livedigital Technologies Private Limited',
+    'maintainer': 'Livedigital Technologies Private Limited',
+    'website': 'https://ldtech.in',
+    'depends': ['base', 'base_setup', 'web', 'auth_signup'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_config_settings_views.xml',
+        'views/login_image.xml',
+        'templates/assets.xml',
+        'templates/left_login_template.xml',
+        'templates/right_login_template.xml',
+        'templates/middle_login_template.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'images': ['static/description/banner.png'],
+}
+
